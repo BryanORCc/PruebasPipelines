76bb6dd56e5936a390daead9e580e7d205cb82d3
Bryan Rodriguez <bryanorcc@outlook.com>
2021-09-19 15:34:41 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebasPipelines.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
